c4d5a034cb12978cad6eb9125172187f8ebb7c37
Abdullah <Abbyboing@gmail.com>
2019-06-26 15:51:18 +0530

    First Step

---
+++ RPS.py
@@ -0,0 +1,22 @@
+ch = 'y'
+while ch == 'y':
+  a = input("Enter the A choice:")
+  b = input("Enter the B choice:")
+  if a == "Rock" and b == "Paper":
+    print("B wins!\n")
+  elif a == "Rock" and b == "Scissors":
+    print("A wins!\n")
+  elif a == "Paper" and b == "Scissors":
+    print("B wins!\n")
+  elif a == "Paper" and b == "Rock":
+    print("A wins!\n")
+  elif a == "Scissors" and b == "Rock":
+    print("B wins!\n")
+  elif a == "Scissors" and b == "Paper":
+    print("A wins!\n")
+  elif a == b:
+    print("Oh! It's a draw\n")
+  else:
+    print("Please use only (Rock/Paper/Scissors)\n")
+  ch = input("Want to continue playing?(y/n)")
+  print("\n")
